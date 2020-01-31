af83ff38f07b22ade70b7960f7ccf3d7317de0cb
Otavio <ottahbl@yahoo.com.br>
2020-01-31 16:15:52 -0300

    Version 3 - with extra

--- files_transfer/files_transfer.py
+++ files_transfer/files_transfer.py
@@ -1,10 +1,8 @@
 import json
 import os
-import shlex
 import shutil
-import subprocess
-import sys
 import time
+import datetime
 from threading import Thread
 
 
@@ -66,3 +64,106 @@ class FileTransfer(Thread):
                             print("File " + file + " transferred successfully")
                         except:
                             "Error"
+
+
+class FileTransferExtra(Thread):
+
+    def __init__(self):
+        super(FileTransferExtra, self).__init__()
+
+        self.current_directory = os.getcwd()
+
+    def run(self):
+
+        while True:
+
+            with open(self.current_directory + '/config.json', 'r') as config_file:
+                reading = config_file.read()
+                data = json.loads(reading)
+
+            source_path = data['config'][0]['source']
+            destiny_path = data['config'][0]['destiny']
+            time_to_wait = data['config'][0]['timer']
+            filter_type = data['config'][0]['filter_type']
+            filter_value = data['config'][0]['filter_value']
+            mode = data['config'][0]['operation_mode']
+
+            print('Your source path is: ')
+            print(source_path)
+
+            print('Your destiny path is: ')
+            print(destiny_path)
+
+            print('os listdir')
+            print(os.listdir(source_path))
+
+            file_list = []
+            for i in os.listdir(source_path):
+                x = os.stat(os.path.join(source_path, i))
+                file_list.append([i, x.st_ctime])
+
+            file_list.sort(key=lambda g: g[1])
+
+            #Transfer oldest file first
+            if mode == '1':
+                for file in file_list:
+
+                    time.sleep(time_to_wait)
+
+                    if filter_type == 'prefix':
+                        if file[0].startswith(filter_value):
+                            # Command to effectively transfer the file
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File "+file[0]+" transferred successfully")
+                            except:
+                                "Error"
+
+                    if filter_type == 'sufix':
+                        if file[0].endswith(filter_value):
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File " + file[0] + " transferred successfully")
+                            except:
+                                "Error"
+
+                    if filter_type == 'infix':
+                        if filter_value in file[0]:
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File " + file[0] + " transferred successfully")
+                            except:
+                                "Error"
+
+            #Transfer newest file first
+            if mode == '2':
+                for file in reversed(file_list):
+
+                    time.sleep(time_to_wait)
+
+                    if filter_type == 'prefix':
+                        if file[0].startswith(filter_value):
+                            # Command to effectively transfer the file
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File "+file[0]+" transferred successfully")
+                            except:
+                                "Error"
+
+                    if filter_type == 'sufix':
+                        if file[0].endswith(filter_value):
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File " + file[0] + " transferred successfully")
+                            except:
+                                "Error"
+
+                    if filter_type == 'infix':
+                        if filter_value in file[0]:
+                            try:
+                                shutil.move(source_path + '/' + file[0], destiny_path + '/' + file[0])
+                                print("File " + file[0] + " transferred successfully")
+                            except:
+                                "Error"
+
+
--- files_transfer/run_files_transfer.py
+++ files_transfer/run_files_transfer.py
@@ -1,7 +1,21 @@
-from files_transfer import FileTransfer
+from files_transfer import FileTransfer, FileTransferExtra
+import json
+import os
+
 
 
 if __name__ == '__main__':
 
-    transfer = FileTransfer()
-    transfer.start()
+    with open(os.getcwd() + '/config.json', 'r') as config_file:
+
+        reading = config_file.read()
+        data = json.loads(reading)
+
+        if data['config'][0]['operation'] == '1':
+            transfer = FileTransfer()
+            transfer.start()
+
+        if data['config'][0]['operation'] == '2':
+            transfer = FileTransferExtra()
+            transfer.start()
+
